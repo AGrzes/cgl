f06e3c371b738ff95306377cbc2f44e53ba27c29
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-09-13 14:33:27 +0200

    Update logic

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,49 @@ function y(coordinates: string): number {
   return Number.parseInt(coordinates.split('x')[1])
 }
 
+type Board = {[key:string]: boolean}
+
+function step(cells: Board):Board{
+  const next: Board = {}
+  const processed: Board = {}
+  function process(x:number,y:number){
+    const coordinates = `${x}x${y}`
+    if (!processed[coordinates]){
+      processed[coordinates] = true
+      let neighbors: number = 0;
+      for (let i=-1;i<=1;i++){
+        for (let j=-1;j<=1;j++){
+          if (i || j) {
+            if (cells[`${x+i}x${y+j}`]){
+              neighbors++
+            }
+          }
+        }
+      }
+      const live = cells[coordinates]
+      if (live && neighbors>=2 && neighbors <= 3){
+        next[coordinates] = true
+      }
+      if (!live && neighbors == 3){
+        next[coordinates] = true
+      }
+    }
+  }
+  for (const coordinates in cells){
+    let xc = x(coordinates)
+    let yc = y(coordinates)
+    for (let i=-1;i<=1;i++){
+      for (let j=-1;j<=1;j++){
+        if (i || j) {
+          process(xc+i,yc+j)
+        }
+      }
+    }
+    process(x(coordinates),y(coordinates))
+  }
+  return next;
+}
+
 Vue.component('cell',{
   props: ['cell'],
   template: `
@@ -40,11 +83,14 @@ Vue.component('cgl',{
   data(){
     return {
       cells: {
-        "0x0": true,
-        "2x3": true,
-        "-2x3": true
+        "1x0": true,
+        "1x1": true,
+        "1x2": true
       }
     }
+  },
+  mounted(){
+    setInterval(()=> this.cells = step(this.cells), 1000)
   }
 })
 
